851f59600a409c3d1047d2ff7c59bc98b6fb39bc
David Biehl <me@davidbiehl.com>
2014-12-11 11:26:12 -0800

    Updates the Categories spec due to the API change from a flat Array to paginated results.

--- spec/api/categories_spec.rb
+++ spec/api/categories_spec.rb
@@ -5,19 +5,19 @@ describe EcwidApi::Api::Categories, faraday: true do
 
   describe "#all" do
     it "gets all of the categories from the client" do
-      expect(client).to receive(:get).with("categories", {}).and_call_original
+      expect(client).to receive(:get).with("categories", hash_including({})).and_call_original
       subject.all
     end
 
     it "gets sub categories" do
-      expect(client).to receive(:get).with("categories", parent: 5).and_call_original
+      expect(client).to receive(:get).with("categories", hash_including(parent: 5)).and_call_original
       subject.all(parent: 5)
     end
   end
 
   describe "#root" do
     it "gets the root level categories" do
-      expect(subject).to receive(:all).with(parent: 0).and_call_original
+      expect(subject).to receive(:all).with(hash_including(parent: 0)).and_call_original
       subject.root
     end
   end
