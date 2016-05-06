4ae4c69944abd9f48a52298e4a3793b56f556424
Maksim Slepukhov <maksim.slepukhov@gmail.com>
2016-05-06 16:56:09 +0400

    Add handling fee field ecwid_reader/writer

--- lib/ecwid_api/order.rb
+++ lib/ecwid_api/order.rb
@@ -13,7 +13,7 @@ module EcwidApi
                  :shippingPerson, :shippingOption, :additionalInfo,
                  :paymentParams, :discountInfo, :trackingNumber,
                  :paymentMessage, :extTransactionId, :affiliateId,
-                 :creditCardStatus
+                 :creditCardStatus, :handlingFee
 
 
     ecwid_writer :subtotal, :total, :email, :paymentMethod, :paymentModule,
@@ -25,7 +25,7 @@ module EcwidApi
                  :items, :billingPerson, :shippingPerson, :shippingOption,
                  :additionalInfo, :paymentParams, :discountInfo,
                  :trackingNumber, :paymentMessage, :extTransactionId,
-                 :affiliateId, :creditCardStatus
+                 :affiliateId, :creditCardStatus, :handlingFee
 
     VALID_FULFILLMENT_STATUSES = %w(
       AWAITING_PROCESSING
