794e1d705c7dc51792bbcb2d95d3a575423dff86
Maksim Slepukhov <maksim.slepukhov@gmail.com>
2016-12-11 23:55:39 +0400

    Add self url root to person entity

--- lib/ecwid_api/person.rb
+++ lib/ecwid_api/person.rb
@@ -1,5 +1,7 @@
 module EcwidApi
   class Person < Entity
+    self.url_root = "persons"
+
     ecwid_reader :name, :companyName, :street, :city, :countryCode,
                  :countryName, :postalCode, :stateOrProvinceCode,
                  :stateOrProvinceName, :phone
