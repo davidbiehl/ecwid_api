ce9130aa9dfee17ff9da8508f097bcd931e65868
Denis Lukyanov <d.lukyanov@secoint.ru>
2019-04-02 16:43:40 +0300

    Access to Customer#billing_person

--- lib/ecwid_api/customer.rb
+++ lib/ecwid_api/customer.rb
@@ -2,8 +2,21 @@ module EcwidApi
   class Customer < Entity
     self.url_root = "customers"
 
-    ecwid_reader :id, :email, :name, :totalOrderCount, :customerGroupId, :customerGroupName, :acceptMarketing
+    ecwid_reader :id, :email, :name, :totalOrderCount, :customerGroupId, :customerGroupName, 
+                 :acceptMarketing, :billingPerson
 
     ecwid_writer :email, :name, :acceptMarketing
+
+    # Public: Returns the billing person
+    #
+    def billing_person
+      build_billing_person
+    end
+
+    private
+
+    def build_billing_person
+      @billing_person ||= data["billingPerson"] && Person.new(data["billingPerson"])
+    end
   end
 end
